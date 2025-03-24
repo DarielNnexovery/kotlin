d24ec666bb63bfbecaf93b80b910893aed731b3e
DarielNnexovery <darielulatelara@gmail.com>
2025-03-24 11:06:18 -0600

    primer trabajo en kotline

---
+++ src/1_Tipos_de_variables/Ejemplos.kt
@@ -0,0 +1,34 @@
+package `1_Tipos_de_variables`
+
+fun main() {
+    // todas las variables val son inmutables
+    // variable inmutable de tipo entero
+    val edad: Int
+    edad = 48
+    //variable inmutable de tipo decimal
+    val sueldo: Float
+    sueldo = 1200.55f // siempre lleva una f al final del valor
+    //variable inmutable de tipo decimal
+    val total: Double
+    total = 70000.24
+    //variable inmutable de tipo alfanumerico
+    val titulo: String
+    titulo = "sistema deventa"
+    //variables mutable, las variables mutables empiezan con "var"
+    var mes: Int
+    mes = 1
+    //algunas lineas mas de codigo
+    mes = 2
+    // variables de tipo Char, almacena un solo caracter
+    var continua: Char = 'S'
+    // variable de tipo Booleano, true o false
+    var fin:Boolean = false
+
+    print("Dariel")
+    print(" Alberto")
+
+    println(" Ulate")
+    println(" Lara")
+
+    print("mes")
+}
---
+++ src/1_Tipos_de_variables/Proyecto2.kt
@@ -0,0 +1,22 @@
+package `1_Tipos_de_variables`
+
+fun main() {
+    /* Crear un programa que defina dos variables inmutables de tipo Int. Luego definir una
+tercer variable mutable que almacene la suma de las dos primeras variables y las
+muestre. Seguidamente almacenar en la variable el producto de las dos primeras
+variables y mostrar el resultado.
+     */
+    val var1: Int
+    val var2: Int
+    var1 = 6
+    var2 = 3
+
+    var producto: Double
+    var respuesta: Double
+
+    producto = 18.0
+    respuesta = producto
+
+    print("el numero es ")
+    print(respuesta)
+}
